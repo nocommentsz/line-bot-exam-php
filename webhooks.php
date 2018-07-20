9948fab3800b11851c5ba1cbd1004cc3655402c2
nocommentsz <41484304+nocommentsz@users.noreply.github.com>
2018-07-21 00:31:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -54,6 +54,14 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    #ตัวอย่าง Message Type "Video"
+    else if($message == "video"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "video";
+        $arrayPostData['messages'][0]['originalContentUrl'] = "https://www.picz.in.th/images/2018/07/21/NXcim8.gif";//ใส่ url ของ video ที่ต้องการส่ง
+        $arrayPostData['messages'][0]['previewImageUrl'] = "";//ใส่รูป preview ของ video
+        replyMsg($arrayHeader,$arrayPostData);
+    }
 
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
