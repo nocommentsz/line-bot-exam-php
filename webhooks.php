2d21023df66786c4991582d26974720f36fc666e
nocommentsz <41484304+nocommentsz@users.noreply.github.com>
2018-07-20 23:23:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xepJPkhdn0jWqgRmXOXDvAZ2vglqyA5xNywc/3/5Xlmh6eBroHqODOmE8C6wSHuIZKGpKGMjASekClEhRceD73tc5bydeipJtKnZCLGvGUlCp4RMl705PwqLq9e/NI24YV0j3R7D1S+DIRc2q9ZZdAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
